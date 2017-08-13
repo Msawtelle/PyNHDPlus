62e0201f30ae294eab09e6b456ead6cf60a82d9f
Msawtelle <mitchell.sawtelle@okstate.edu>
2017-08-13 05:33:51 -0500

    added setup.py, __init__.py, license.txt, renamed class file

---
+++ NHDPlus_Extractor/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='PyNHDPLUS',
+      version='1.0',
+      description='',
+      url='https://github.com/Msawtelle/PyNHDPLUS',
+      author='Mitchell Sawtelle',
+      author_email='mitchell.sawtelle@okstate.edu',
+      license='GNU',
+      packages=['NHDPlus_Extractor'],
+      install_requires=['subprocess','time', 'os', 'numpy', 'struct'.'datetime'
+                        'shutil', 'osgeo', 'urllib.request', 'urllib.error',
+                        'gdalconst','matplotlib','shapefile','pickle',
+                        'mpl_toolkits.axes_grid1'
+                        ]
+      zip_safe=False)
