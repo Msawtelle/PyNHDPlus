a57eeea094142e4206455d92bc29a25205f3292b
Msawtelle <mitchell.sawtelle@okstate.edu>
2017-08-14 13:08:14 -0500

    added description

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup
 
 setup(name='PyNHDPLUS',
-      version='1.0',
-      description='',
+      version='1.1',
+      description='tools to download and manipulate the nhdplus data set.',
       url='https://github.com/Msawtelle/PyNHDPLUS',
       author='Mitchell Sawtelle',
       author_email='mitchell.sawtelle@okstate.edu',
       license='GNU',
       packages=['NHDPlus_Extractor'],
-      install_requires=['subprocess','time', 'os', 'numpy', 'struct','datetime'
-                        'shutil', 'osgeo', 'urllib.request', 'urllib.error',
+      install_requires=['numpy','osgeo', 'urllib.request', 'urllib.error',
                         'gdalconst','matplotlib','shapefile','pickle',
                         'mpl_toolkits.axes_grid1'
                         ],
